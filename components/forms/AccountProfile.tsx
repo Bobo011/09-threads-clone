e2775eb519ffd51eb93aaccfc58cfedca4135693
George <alximik15@gmail.com>
2023-09-13 18:20:22 +0400

    Testing Uploading data

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -21,7 +21,9 @@ import { ChangeEvent, useState } from "react";
 
 import { isBase64Image } from "@/lib/utils";
 import { useUploadThing } from "@/lib/uploadthing";
+import { updateUser } from "@/lib/actions/user.actions";
 
+import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
   user: {
@@ -37,7 +39,9 @@ interface Props {
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
   const [files, setFiles] = useState<File[]>([]);
-  const {startUpload} = useUploadThing('media')
+  const { startUpload } = useUploadThing("media");
+  const router = useRouter();
+  const pathname = usePathname();
 
   const form = useForm({
     resolver: zodResolver(UserValidation),
@@ -66,30 +70,43 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 
       fileReader.onload = async (event) => {
         const imageDataUrl = event.target?.result?.toString() || "";
- 
+
         fieldChange(imageDataUrl);
       };
 
-	  fileReader.readAsDataURL(file)
+      fileReader.readAsDataURL(file);
     }
   };
 
-   const onSubmit = async(values: z.infer<typeof UserValidation>) =>{
- const blob = values.profile_photo
+  const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+    const blob = values.profile_photo;
 
-const hasImageChanged = isBase64Image(blob)
+    const hasImageChanged = isBase64Image(blob);
 
-if(hasImageChanged){
-const imgRes = await startUpload(files);
+    if (hasImageChanged) {
+      const imgRes = await startUpload(files);
 
-if (imgRes && imgRes[0].url) {
-  values.profile_photo = imgRes[0].url;
+      if (imgRes && imgRes[0].url) {
+        values.profile_photo = imgRes[0].url;
+      }
+    }
+
+    await updateUser({
+      userId: user.id,
+      username: values.username,
+      name: values.name,
+      bio: values.bio,
+      image: values.profile_photo,
+      path: pathname,
+    });
+
+if(pathname === '/profile/edit'){
+  router.back();
+}else{
+  router.push('/')
 }
-}
 
-// TODO: Update user profile 
-
-  }
+  };
 
   return (
     <Form {...form}>
