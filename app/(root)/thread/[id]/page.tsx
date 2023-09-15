ddd18b36f2b2a44ef055107ce48e524e342f1f36
George <alximik15@gmail.com>
2023-09-15 14:08:56 +0400

    Adding Comments finished

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -19,30 +19,47 @@ if (!userInfo?.onboarded) redirect("/onboarding");
 
 const thread = await fetchThreadById(params.id)
 
-return(
+return (
+  <section className="relative">
+    <div>
+      <ThreadCard
+        key={thread._id}
+        id={thread._id}
+        currentUserId={user?.id || ""}
+        parentId={thread.parentId}
+        content={thread.text}
+        author={thread.author}
+        community={thread.community}
+        createdAt={thread.createdAt}
+        comments={thread.children}
+      />
+    </div>
+    <div className="mt-7">
+      <Comment
+        threadId={thread.id}
+        currentUserImg={userInfo.image}
+        currentUserId={JSON.stringify(userInfo._id)}
+      />
+    </div>
 
-<section className="relative">
-  <div>
-    <ThreadCard
-      key={thread._id}
-      id={thread._id}
-      currentUserId={user?.id || ""}
-      parentId={thread.parentId}
-      content={thread.text}
-      author={thread.author}
-      community={thread.community}
-      createdAt={thread.createdAt}
-      comments={thread.children}
-    />
-  </div>
-  <Comment
- threadId = {thread.id} 
- currentUserImg = {user.imageUrl}
- currentUserId={JSON.stringify(userInfo._id)}
-  />
-</section>
-
-)
+    <div className="mt-10">
+      {thread.children.map((childItem: any) => (
+        <ThreadCard
+          key={childItem._id}
+          id={childItem._id}
+          currentUserId={user?.id || ""}
+          parentId={childItem.parentId}
+          content={childItem.text}
+          author={childItem.author}
+          community={childItem.community}
+          createdAt={childItem.createdAt}
+          comments={childItem.children}
+          isComment
+        />
+      ))}
+    </div>
+  </section>
+);
 }
 
 export default Page
