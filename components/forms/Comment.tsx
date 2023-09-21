3fa9efdd1befa32c01d595c6d6b6a4c22e28630a
George <alximik15@gmail.com>
2023-09-21 23:23:29 +0400

    Change

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -1,45 +1,87 @@
 "use client";
 
+import { z } from "zod";
 import Image from "next/image";
-import { usePathname, useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { usePathname } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-import { deleteThread } from "@/lib/actions/thread.actions";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+import { CommentValidation } from "@/lib/validations/thread";
+import { addCommentToThread } from "@/lib/actions/thread.actions";
 
 interface Props {
   threadId: string;
+  currentUserImg: string;
   currentUserId: string;
-  authorId: string;
-  parentId: string | null;
-  isComment?: boolean;
 }
 
-function DeleteThread({
-  threadId,
-  currentUserId,
-  authorId,
-  parentId,
-  isComment,
-}: Props) {
+function Comment({ threadId, currentUserImg, currentUserId }: Props) {
   const pathname = usePathname();
-  const router = useRouter();
 
-  if (currentUserId !== authorId || pathname === "/") return null;
+  const form = useForm<z.infer<typeof CommentValidation>>({
+    resolver: zodResolver(CommentValidation),
+    defaultValues: {
+      thread: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof CommentValidation>) => {
+    await addCommentToThread(
+      threadId,
+      values.thread,
+      JSON.parse(currentUserId),
+      pathname
+    );
+
+    form.reset();
+  };
 
   return (
-    <Image
-      src="/assets/delete.svg"
-      alt="delte"
-      width={18}
-      height={18}
-      className="cursor-pointer object-contain"
-      onClick={async () => {
-        await deleteThread(JSON.parse(threadId), pathname);
-        if (!parentId || !isComment) {
-          router.push("/");
-        }
-      }}
-    />
+    <Form {...form}>
+      <form className="comment-form" onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+            <FormItem className="flex w-full items-center gap-3">
+              <FormLabel>
+                <Image
+                  src={currentUserImg}
+                  alt="current_user"
+                  width={48}
+                  height={48}
+                  className="rounded-full object-cover"
+                />
+              </FormLabel>
+              <FormControl className="border-none bg-transparent">
+                <Input
+                  type="text"
+                  {...field}
+                  placeholder="Comment..."
+                  className="no-focus text-light-1 outline-none"
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" className="comment-form_btn">
+          Reply
+        </Button>
+      </form>
+    </Form>
   );
 }
 
-export default DeleteThread;
+export default Comment;
